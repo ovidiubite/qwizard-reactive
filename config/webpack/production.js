3f2420b4d6a94651da61f786555b4f6da56f7e31
Horia Radu <horia.radu23@gmail.com>
2021-05-20 10:18:33 +0300

    Fix starting in production mode

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,32 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+var path = require("path");
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const environment = require('./environment')
+const environment = require("./environment");
 
-module.exports = environment.toWebpackConfig()
+// Get the actual sass-loader config
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function(element) {
+  return element.loader === 'sass-loader'
+})
+
+// Use Dart-implementation of Sass (default is node-sass)
+const options = sassLoaderConfig.options
+options.implementation = require('sass')
+
+environment.loaders.prepend('sass', {
+  test: /.s(a|c)ss$/,
+  use: [
+    "style-loader",
+    {
+      loader: "css-loader",
+      options: {
+        importLoaders: 1,
+      },
+    },
+    "sass-loader",
+    "postcss-loader"
+  ]
+})
+
+
+module.exports = environment.toWebpackConfig();

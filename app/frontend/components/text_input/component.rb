4c361a1d08716367985df55cd43955ef3e9687b5
Horia Radu <horia.radu23@gmail.com>
2021-07-09 11:59:24 +0300

    Show error messages inline (#85)

--- app/frontend/components/text_input/component.rb
+++ app/frontend/components/text_input/component.rb
@@ -3,4 +3,17 @@ class TextInput::Component < ApplicationViewComponent
   option :field_name
   option :label_text, default: proc { '' }
   option :placeholder, default: proc { '' }
+
+  # can be inline
+  option :variant, default: proc { '' }
+
+  option :input_data_attributes, default: proc { {} }
+
+  def container_class
+    @variant == 'inline' ? 'inlineInputContainer' : 'inputContainer'
+  end
+
+  def input_class
+    @variant == 'inline' ? 'inlineInput' : 'input'
+  end
 end

43c4fb55be3cb0e8ac487b89bd033294aa79336b
Horia Radu <horia.radu23@gmail.com>
2021-07-12 14:51:46 +0300

    multiple suggest question works

--- app/frontend/components/question_details/index.js
+++ app/frontend/components/question_details/index.js
@@ -3,8 +3,8 @@ import "./index.scss";
 import {Controller as BaseController} from "stimulus";
 
 export class Controller extends BaseController {
-  connect() {
-    // ...
+  reset() {
+    this.element.src = '';
   }
 }
 

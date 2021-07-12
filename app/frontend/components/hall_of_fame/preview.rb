631390fc55124a8250557c2fdffaf297830dc44e
ghdenisa <gherghiceanudenisa@yahoo.com>
2021-07-12 12:58:29 +0300

    Fix hat size (#94)
    Co-authored-by: Denisa Gherghiceanu <denisa.gherghiceanu@unifiedpost.com>

--- app/frontend/components/hall_of_fame/preview.rb
+++ app/frontend/components/hall_of_fame/preview.rb
@@ -1,5 +1,5 @@
 class HallOfFame::Preview < ApplicationViewComponentPreview
   def default
-    render(HallOfFame::Component.new(Lobby.first, Player.first))
+    render(HallOfFame::Component.new(lobby: Lobby.first, current_player_id: Player.first.id))
   end
 end
